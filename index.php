b9e34593756c425c6213068a61b0a48bb17fea5f
Ajay4653 <107074680+Ajay4653@users.noreply.github.com>
2023-01-30 14:39:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,7 @@
 <h1> openshift application </h1>
 
 <?php
-echo " HELLO THIS IS MY FIRST OPENSHIFT APPLICATION (AJAY SHINDE)";
-  echo "Hello This is WEBHOOK CREATED BY ME FOR AUTOMATIC TRIGGER thank you";
-   echo "HELLO THIS IS NEW CODE thank you";
+echo "HELLO THIS IS WEBHOOK CREATED BY AJAY SHINDE";
 ?>
 
 </body>
